c082c0cdbfdd6af25a581d8ea8d6e3bdc93cfef0
Lele97 <gabriele.grandinetti@hotmail.com>
2023-07-10 19:29:16 +0200

    Frist commit test

---
+++ src/main/java/com/udemi/corso/spring/guru/spring5WebApp/Spring5WebAppApplication.java
@@ -0,0 +1,13 @@
+package com.udemi.corso.spring.guru.spring5WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebAppApplication.class, args);
+	}
+
+}
