8bdd5375ab022212dbda7389c65d6e42f4e4f4ea
Gabriele Grandinetti <gabriele.grandinetti@hotmail.com>
2024-01-11 21:22:49 +0100

    Delete spring5WebApp directory
    Delete spring5WebApp directory

--- spring5WebApp/src/main/java/com/udemi/corso/spring/guru/spring5WebApp/repositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.udemi.corso.spring.guru.spring5WebApp.repositories;
-
-import com.udemi.corso.spring.guru.spring5WebApp.domain.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-}
